82387803b019468f4888816b7e2d3c682c224cc6
NahanFrota <140098966+NahanFrota@users.noreply.github.com>
2025-02-22 04:41:18 -0300

    sistemas de multas

--- site-exemplo/olamundo.js
+++ site-exemplo/olamundo.js
@@ -1,1 +1,11 @@
-alert("asdasd")
+function clicar(){
+    var a =  document.getElementById('number1')
+    var b =  Number(a.value)
+    if (b > 60){
+       console.log(" voce foi multado")
+    } else{
+        console.log("nao foi multado")
+        
+    }
+
+}
